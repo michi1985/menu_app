24fcf712895f0923d629557be064d214be038b54
m.ishida <acoustic.revolution0322@gmail.com>
2025-12-19 12:48:52 +0900

    importとexportの自動ソートのためにeslint-plugin-simple-import-sortを追加、eslint.config.mjsに記述を追加
    npm i -D eslint-plugin-simple-import-sort

--- user_frontend/eslint.config.mjs
+++ user_frontend/eslint.config.mjs
@@ -2,10 +2,20 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettier from "eslint-config-prettier/flat"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
   prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
